1d6c1e5e5fa478e8795a57547fd490e90b7a7b30
hddevteam <mengmengxiao@msn.com>
2017-03-29 16:26:19 +0800

    Add files via upload
    you may use the file to send code via the json code file exported from the rmbridge.
    usage: sendcode.py codename

---
+++ sendcode.py
@@ -0,0 +1,47 @@
+#you may use the file to send code via the json code file exported from the rmbridge.
+#usage: sendcode.py codename
+import sys
+import json
+import broadlink
+
+#modify the file name to your code file exported from rmbridge the file content shoud like the following
+jsonFile = 'code.json'
+
+#modify the ip address and the mac address with your device.
+device_ip = "192.168.1.57"
+device_mac = "34ea34e398a7"
+
+device_type = "broadlink.rm2"
+device_port = 80
+
+device = broadlink.rm(host=(device_ip,device_port), mac=bytearray.fromhex(device_mac))
+
+device.auth()
+device.host
+print 'host: ' + ' '.join(str(v) for v in device.host)
+
+codeName = ''
+if len(sys.argv)>1: #check if user has input the code name.
+    #print len(sys.argv)
+    codeName = sys.argv[1]
+    codeName = codeName.strip()
+if codeName!='':
+    isfound = False
+    with open(jsonFile) as json_data:
+        jsonData = json.load(json_data)
+    #print(jsonData)
+    #print len(json.loads(data))
+    for jd in jsonData:  #Traverse and find the code with name.
+        #print jd
+        if jd.get("name") == codeName: #compare name
+            codeData = jd.get("data")
+            isfound = True
+            print jd.get("name")
+            print codeData
+            device.send_data(codeData.decode('hex'))
+            print 'Data sent'
+    if not isfound:
+        print 'Can not find the code via codeName:', codeName, '. Please check it.'
+    #device.send_data(codeData.decode('hex'))
+else:
+    print 'received no codeName'
